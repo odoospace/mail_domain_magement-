e6113837345a248b5e82787b552b96aee71d8760
alfonsomb <alfonso@impulso.xyz>
2021-07-27 17:34:56 +0200

    Restrict mailing when creating

--- models/models.py
+++ models/models.py
@@ -6,14 +6,7 @@ class DomainToRestrict(models.Model):
     _name = 'domain.to.restrict'
 
     name = fields.Char('Domain name', required=True)
-
-
-class DomainRestriction(models.Model):
-    _name = 'domain.restriction'
-
-    name = fields.Char('Name', required=True)
     active = fields.Boolean('Active', default=True)
-    domains_ids = fields.Many2many('domain.to.restrict', string='Domains')
 
 
 class MailMail(models.Model):
@@ -21,22 +14,27 @@ class MailMail(models.Model):
 
     @api.model
     def create(self, values):
-        mail = super(MailMail, self).create(values)
-        domain_restriction_obj = self.env['domain.restriction']
-        enc = False
+        domain_to_restrict_obj = self.env['domain.to.restrict']
+        domain_restriction_list = [domain_restriction.name for domain_restriction in
+                                   domain_to_restrict_obj.search([('active', '=', True)])]
 
-        if mail.email_to:
-            for domain_restriction in domain_restriction_obj.search([('active', '=', True)]):
-                for domain in domain_restriction.domains_ids:
-                    if domain.name in mail.email_to:
-                        enc = True
-                        break
+        if 'email_to' in values:
+            enc = False
+            for domain_restriction in domain_restriction_list:
+                if domain_restriction in values['email_to']:
+                    enc = True
+                    break
+            if not enc:
+                values['email_to'] = ''
+        else:
+            if 'recipient_ids' in values and values['recipient_ids']:
+                recipients_list = []
+                for recipient_id in values['recipient_ids']:
+                    recipient = self.env['res.partner'].browse(recipient_id[1])
+                    if recipient.email:
+                        for domain_restriction in domain_restriction_list:
+                            if domain_restriction in recipient.email:
+                                recipients_list.append((4, recipient.id))
+                values['recipient_ids'] = recipients_list
 
-        if mail.recipients_ids:
-            print('Send Mail!')
-
-        if enc:
-            raise Warning(
-                _('The domain of the destination email %s is not included in the list of allowed emails') % mail.email_to)
-
-        return mail
+        return super(MailMail, self).create(values)
