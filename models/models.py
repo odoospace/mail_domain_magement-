67174ae56fb462dec34474f0734c8390b92e82ef
alfonsomb <alfonso@impulso.xyz>
2021-07-28 09:47:54 +0200

    Fix Bug when Recipients is Tuple or List

--- models/models.py
+++ models/models.py
@@ -18,7 +18,7 @@ class MailMail(models.Model):
         domain_restriction_list = [domain_restriction.name for domain_restriction in
                                    domain_to_restrict_obj.search([('active', '=', True)])]
 
-        if 'email_to' in values:
+        if 'email_to' in values and values['email_to']:
             enc = False
             for domain_restriction in domain_restriction_list:
                 if domain_restriction in values['email_to']:
@@ -28,13 +28,24 @@ class MailMail(models.Model):
                 values['email_to'] = ''
         else:
             if 'recipient_ids' in values and values['recipient_ids']:
-                recipients_list = []
-                for recipient_id in values['recipient_ids']:
-                    recipient = self.env['res.partner'].browse(recipient_id[1])
-                    if recipient.email:
-                        for domain_restriction in domain_restriction_list:
-                            if domain_restriction in recipient.email:
-                                recipients_list.append((4, recipient.id))
-                values['recipient_ids'] = recipients_list
+                if isinstance(values['recipient_ids'][0], tuple):
+                    recipients_list = []
+                    for recipient_id in values['recipient_ids']:
+                        recipient = self.env['res.partner'].browse(recipient_id[1])
+                        if recipient.email:
+                            for domain_restriction in domain_restriction_list:
+                                if domain_restriction in recipient.email:
+                                    recipients_list.append((4, recipient.id))
+                    values['recipient_ids'] = recipients_list
+                elif isinstance(values['recipient_ids'][0], list):
+                    recipients_list = []
+                    for recipient_id in values['recipient_ids'][0][2]:
+                        recipient = self.env['res.partner'].browse(recipient_id)
+                        if recipient.email:
+                            for domain_restriction in domain_restriction_list:
+                                if domain_restriction in recipient.email:
+                                    recipients_list.append(recipient.id)
+                    values['recipient_ids'] = [[6, 0, recipients_list]]
+
 
         return super(MailMail, self).create(values)
